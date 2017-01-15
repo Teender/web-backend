dfce5db8eb92d82e5fbf2895bbac25374ca17ff1
Marc Bacvanski <marc.bacvanski@gmail.com>
2017-01-14 17:08:42 -0800

    Sending email my bois!

--- router.js
+++ router.js
@@ -2,4 +2,36 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     res.status(200).send({message: 'o hai'});
   });
+
+  app.post('/sendEmail', (req, res) => {
+    let emailValidator = require('email-validator');
+    let email = req.body.email;
+    let school = req.body.school;
+    let message = req.body.message;
+
+    if (!emailValidator.validate(email)) {
+      res.status(400).send({error: 'invalid email'});
+    } else {
+
+
+      let send = require('gmail-send')({
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASSWORD,
+        to: email,
+        subject: 'An Anonymous Admirer Invites You to Join Teender',
+        text: 'Hey there, beautiful! An anonymous friend from ' + school + ' on Facebook wants you to join Teender, ' +
+        'a social app for teens around the world. ' + message + '\n Download the app for iOS: ' +
+        'google.com/foobar \n Download the app for Android: google.com/foobar',
+      });
+
+      // Override any default option and send email
+      send({}, function (err, response) {
+        if (!err) {
+          res.status(200).send({response: response});
+        } else {
+          res.status(400).send({error: err});
+        }
+      });
+    }
+  });
 };
