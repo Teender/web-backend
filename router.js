c552b82688cdcdf09f9170f412267e4b0fb4efed
Marc Bacvanski <marc.bacvanski@gmail.com>
2017-01-14 21:54:06 -0800

    rollback to good times

--- router.js
+++ router.js
@@ -15,32 +15,25 @@ module.exports = (app) => {
       res.status(400).send({error: 'invalid email'});
     } else {
       let fs = require('fs');
-      fs.readFile('emailFile.html', (err, file) => {
-        if (!err) {
-          let send = require('gmail-send')({
-            user: process.env.GMAIL_USERNAME,
-            pass: process.env.GMAIL_PASSWORD,
-            to: email,
-            subject: 'An Anonymous Admirer Invites You to Join Teender',
-            // text: 'Hey there, beautiful! An anonymous friend from ' + school + ' on Facebook wants you to join Teender, ' +
-            // 'a social app for teens around the world. ' + message + '\n Download the app for iOS: ' +
-            // 'google.com/foobar \n Download the app for Android: google.com/foobar',
-            html: file
-          });
+      let send = require('gmail-send')({
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASSWORD,
+        to: email,
+        subject: 'An Anonymous Admirer Invites You to Join Teender',
+        text: 'Hey there, beautiful! An anonymous friend from ' + school + ' on Facebook wants you to join Teender, ' +
+        'a social app for teens around the world. ' + message + '\n Download the app for iOS: ' +
+        'google.com/foobar \n Download the app for Android: google.com/foobar',
+      });
 
-          // Override any default option and send email
-          send({}, function (err, response) {
-            if (!err) {
-              res.status(200).send({response: response});
-            } else {
-              res.status(400).send({error: err});
-            }
-          });
+      // Override any default option and send email
+      send({}, function (err, response) {
+        if (!err) {
+          res.status(200).send({response: response});
         } else {
-          console.error('o faq ' + err);
-          res.status(500).send();
+          res.status(400).send({error: err});
         }
       });
     }
   });
+
 };
